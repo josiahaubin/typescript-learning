20e710540267e268380a35fea8b06a214cffb964
josiahaubin <josiahaubin@gmail.com>
2021-12-16 15:18:37 -0700

    void, any, never, and unknown

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,30 @@
 //NOTE functions
-// const getFullName = (name: string, surname: string): string => {
-//     return name + " " + surname;
-// }
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Josiah", "Aubin"));
+var user = {
+    name: "Monster",
+    age: 30,
+    getMessage: function () {
+        return 'Hello' + name;
+    }
+};
+var user2 = {
+    name: "Jack",
+    getMessage: function () {
+        return 'Hello' + name;
+    }
+};
+console.log(user.name);
 var popularTags = ["dragon", "coffee"];
 var dragons = "dragon"; //can be null as well
 var username = "alex";
 var pagename = "1";
 var errorMessage = null;
-var user = null;
+var newUser = null;
+//NOTE void, any (don't use), never (not very useful), unknown
+var doSomething = function () {
+    console.log("doSomething");
+};
+var foo = "foo";
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,40 @@
 //NOTE functions
-// const getFullName = (name: string, surname: string): string => {
-//     return name + " " + surname;
-// }
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+}
 
-// console.log(getFullName("Josiah", "Aubin"));
+console.log(getFullName("Josiah", "Aubin"));
 
 //NOTE interfaces
 //? makes it not mandatory
-// interface IUser {
-//     name: string;
-//     age?: number;
-//     getMessage(): string
-// }
-// const user: IUser = {
-//     name: "Monster",
-//     age: 30,
-//     getMessage() {
-//         return 'Hello' + name;
-//     }
-// };
+interface IUser {
+    name: string;
+    age?: number;
+    getMessage(): string
+}
+const user: IUser = {
+    name: "Monster",
+    age: 30,
+    getMessage() {
+        return 'Hello' + name;
+    }
+};
 
-// const user2: IUser = {
-//     name: "Jack",
-//     getMessage() {
-//         return 'Hello' + name;
-//     }
-// };
+const user2: IUser = {
+    name: "Jack",
+    getMessage() {
+        return 'Hello' + name;
+    }
+};
 
-// console.log(user.name)
+console.log(user.name)
 
 //NOTE Union and Type
 type ID = string;
 type PopularTag = string;
 type MaybePopularTag = PopularTag | null;
 
-interface IUser {
+interface UserInterface {
     id: ID
     name: string
     surname: string
@@ -45,4 +45,11 @@ const dragons: MaybePopularTag = "dragon"; //can be null as well
 let username: string = "alex";
 let pagename: string | number = "1";
 let errorMessage: string | null = null;
-let user: IUser | null = null;
+let newUser: UserInterface | null = null;
+
+//NOTE void, any (don't use), never (not very useful), unknown
+const doSomething = (): void => {
+    console.log("doSomething");
+}
+
+let foo: any = "foo";
