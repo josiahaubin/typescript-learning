bc7dd11fcaa54113c47cc420024bcc8ab51915db
josiahaubin <josiahaubin@gmail.com>
2021-12-16 14:26:18 -0700

    commit

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,18 @@
-var a = "1";
-console.log('aaa', a);
+//NOTE functions
+// const getFullName = (name: string, surname: string): string => {
+//     return name + " " + surname;
+// }
+var user = {
+    name: "Monster",
+    age: 30,
+    getMessage: function () {
+        return 'Hello' + name;
+    }
+};
+var user2 = {
+    name: "Jack",
+    getMessage: function () {
+        return 'Hello' + name;
+    }
+};
+console.log(user.name);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-const a = "1";
-console.log('aaa', a)
+//NOTE functions
+// const getFullName = (name: string, surname: string): string => {
+//     return name + " " + surname;
+// }
+
+// console.log(getFullName("Josiah", "Aubin"));
+
+//NOTE interfaces
+//? makes it not mandatory
+interface IUser {
+    name: string;
+    age?: number;
+    getMessage(): string
+}
+const user: IUser = {
+    name: "Monster",
+    age: 30,
+    getMessage() {
+        return 'Hello' + name;
+    }
+};
+
+const user2: IUser = {
+    name: "Jack",
+    getMessage() {
+        return 'Hello' + name;
+    }
+};
+
+console.log(user.name)
